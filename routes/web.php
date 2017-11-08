41a378fdd92ec10204249631b3f873e39855c775
Aleksander Drozd <aleksander.drozd@outlook.com>
2017-11-08 14:49:33 +0100

    Add navbar

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', 'TournamentController@index');
+Route::get('/', function () {
+    return redirect('/home');
+});
+Route::get('/home', 'TournamentController@index') -> name('home');
 Route::get('/tournaments/{tournament}', 'TournamentController@show');
 
 Route::get('/users/{id}', 'UserController@show');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
