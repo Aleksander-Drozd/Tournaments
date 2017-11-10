89a211fb3784473c762447c5afca82001cfc3611
Aleksander Drozd <aleksander.drozd@outlook.com>
2017-11-10 19:48:07 +0100

    Implement /me page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,18 +4,37 @@ namespace App\Http\Controllers;
 
 use Auth;
 use App\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use App\Tournament;
 
 class UserController extends Controller {
 
-    function show(User $user) {
+    public function __construct() {
+        $this -> middleware('auth', ['only' => 'me']);
+    }
+
+    public function show(User $user) {
         return view('user');
     }
 
-    function me() {
-        $tournaments = Tournament::active() -> get();
+    public function me() {
         $user = Auth::user();
-        return view('user.index', compact('user', 'tournaments'));
+        $activeOrganizedTournaments = $user -> activeOrganizedTournaments() -> get();
+        $pastOrganizedTournaments = $user -> pastOrganizedTournaments() -> get();
+        $currentlyParticipatingIn = $user -> currentlyParticipatingIn() -> get();
+        $this -> determineOpponents($currentlyParticipatingIn, $user);
+        $participatedIn = $user -> participatedIn() -> get();
+
+        return view('user.index', compact('user', 'activeOrganizedTournaments',
+            'pastOrganizedTournaments', 'currentlyParticipatingIn', 'participatedIn'));
+    }
+
+    private function determineOpponents(Collection $tournaments, User $user) {
+        foreach ($tournaments as $tournament) {
+            foreach ($tournament -> matches as $match) {
+                $match -> determineOpponentForUser($user);
+            }
+        }
     }
 }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,16 +6,47 @@
     <div class="container">
         @include('user.partials.userInfo', ['user' => $user])
         <div class="container">
-            <h1>Tournaments</h1>
-            @if(count($tournaments) > 0)
-                <div>
-                    @foreach($tournaments as $tournament)
-                        @include('layouts.partials.tournamentWithMatch', ['tournament' => $tournament])
+            <h1>Tournaments currently being organized by you</h1>
+            <ul>
+                @foreach($activeOrganizedTournaments as $tournament)
+                    <li>{{ $tournament -> name }}</li>
+                @endforeach
+            </ul>
+            <h1>Past organized tournament</h1>
+            <ul>
+                @foreach($pastOrganizedTournaments as $tournament)
+                    <li>{{ $tournament -> name }}</li>
+                @endforeach
+            </ul>
+            <h1>Participating in active</h1>
+            <ul>
+                @foreach($currentlyParticipatingIn as $tournament)
+                    <li>{{ $tournament -> name }}</li>
+                    <ul>
+                    @foreach($tournament -> matches as $match)
+                            <li>Match against {{ $match -> opponent -> name }} <br>
+                                Date: {{ $match -> datetime }} <br>
+                                Location: {{ $match -> location }}
+                            </li>
                     @endforeach
-                </div>
-            @else
-                <h2>There are no tournaments you are participating in.</h2>
-            @endif
+                    </ul>
+                @endforeach
+            </ul>
+            <h1>Participated in</h1>
+            <ul>
+                @foreach($participatedIn as $tournament)
+                    <li>{{ $tournament -> name }}</li>
+                @endforeach
+            </ul>
+            {{--@if(count($tournaments) > 0)--}}
+                {{--<div>--}}
+                    {{--@foreach($tournaments as $tournament)--}}
+                        {{--@include('layouts.partials.tournamentWithMatch', ['tournament' => $tournament])--}}
+                    {{--@endforeach--}}
+                {{--</div>--}}
+            {{--@else--}}
+                {{--<h2>There are no tournaments you are participating in.</h2>--}}
+            {{--@endif--}}
         </div>
     </div>
 @endsection
