1a5ca1c5392b4c5d64b0110f2ff329b559d1ce0c
Jakub Małyjasiak <malyjasiak@gmail.com>
2017-12-04 06:45:17 +0100

    Remove tournaments list header if empty.

--- resources/views/tournaments/index.blade.php
+++ resources/views/tournaments/index.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <link rel="stylesheet" href="/css/tournament.css">
     <div class="container">
-        <h1>Active tournaments</h1>
         @if(count($activeTournaments) > 0)
+            <h1>Active tournaments</h1>
             <div>
                 @foreach($activeTournaments as $tournament)
                     @include('layouts.partials.tournament', ['tournament' => $tournament])
@@ -15,15 +15,15 @@
             <h2>There are no active tournaments being played in the moment</h2>
         @endif
 
-        <h1>Upcoming tournaments</h1>
         @if(count($futureTournaments) > 0)
-            <div>
-                @foreach($futureTournaments as $tournament)
-                    @include('layouts.partials.tournament', ['tournament' => $tournament])
-                @endforeach
-            </div>
-        @else
-            <h3>There are no upcoming tournaments</h3>
-        @endif
+                <h1>Upcoming tournaments</h1>
+                <div>
+                    @foreach($futureTournaments as $tournament)
+                        @include('layouts.partials.tournament', ['tournament' => $tournament])
+                    @endforeach
+                </div>
+            @else
+                <h3>There are no upcoming tournaments</h3>
+            @endif
     </div>
 @endsection
