8ea00361c0703e0637725ff8791fde1cdd5186b0
Jakub Małyjasiak <malyjasiak@gmail.com>
2017-12-05 16:45:22 +0100

    Add my-matches view.

--- resources/views/layouts/partials/matchAgainst.blade.php
+++ resources/views/layouts/partials/matchAgainst.blade.php
@@ -1,6 +1,6 @@
 <div class="row col-md-offset-2">
     <div class="col-md-6">
-        <h4>Match against: {{ $match -> opponent -> name}}</h4>
+        <h4>Match against: {{ $match -> opponent(Auth::user()) -> name}}</h4>
     </div>
     <div class="col-md-3">
         <h5>{{ $match -> datetime}}</h5>
--- resources/views/singleTournament/tab-panes/my-matches.blade.php
+++ resources/views/singleTournament/tab-panes/my-matches.blade.php
@@ -1,10 +1,20 @@
 <div>
     @auth()
-        @include('singleTournament.partials.nextMatchFillable', ['nextMatch' => $matches[0]])
+        @include('singleTournament.partials.nextMatchFillable', ['nextMatch' => $tournament -> matches[0]])
         @if(count($matches) > 0)
-            <h4>Upcoming matches</h4>
+            <h3 style="margin-top: 60px">Upcoming matches</h3>
             @foreach($matches as $match)
-                @include('layouts.partials.matchAgainst', ['match' => $match])
+                <div class="row">
+                    <div class="col-md-6">
+                        <h4>Match against: {{ $match -> opponent(Auth::user()) -> name}}</h4>
+                    </div>
+                    <div class="col-md-3">
+                        <h5>{{ $match -> datetime}}</h5>
+                    </div>
+                    <div class="col-md-3">
+                        <h5>{{$match -> location}}</h5>
+                    </div>
+                </div>
             @endforeach
         @endif
     @endauth
