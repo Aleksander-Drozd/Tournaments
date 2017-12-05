c5b04653305268ce707f905ee7ceea18086920d9
Jakub Małyjasiak <malyjasiak@gmail.com>
2017-12-05 16:48:13 +0100

    Mkae use of $userMatches.

--- resources/views/singleTournament/tab-panes/my-matches.blade.php
+++ resources/views/singleTournament/tab-panes/my-matches.blade.php
@@ -1,9 +1,9 @@
 <div>
     @auth()
-        @include('singleTournament.partials.nextMatchFillable', ['nextMatch' => $tournament -> matches[0]])
-        @if(count($matches) > 0)
+        @if(count($userMatches) > 0)
+            @include('singleTournament.partials.nextMatchFillable', ['nextMatch' => $userMatches[0]])
             <h3 style="margin-top: 60px">Upcoming matches</h3>
-            @foreach($matches as $match)
+            @foreach($userMatches as $match)
                 <div class="row">
                     <div class="col-md-6">
                         <h4>Match against: {{ $match -> opponent(Auth::user()) -> name}}</h4>
