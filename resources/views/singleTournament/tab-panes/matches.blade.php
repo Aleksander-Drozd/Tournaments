0b0723ad708e388e1a603be6779d71a86e715387
Jakub Małyjasiak <malyjasiak@gmail.com>
2017-12-05 17:57:00 +0100

    Add headers.

--- resources/views/singleTournament/tab-panes/matches.blade.php
+++ resources/views/singleTournament/tab-panes/matches.blade.php
@@ -1,5 +1,14 @@
 <div>
     <ul>
+        <div class="col-md-6">
+            <h5 style="font-weight: bold">Players and score</h5>
+        </div>
+        <div class="col-md-3">
+            <h5 style="font-weight: bold">Date</h5>
+        </div>
+        <div class="col-md-3">
+           <h5 style="font-weight: bold">Location</h5>
+        </div>
         @foreach($tournament -> matches as $match)
             @include('layouts.partials.match', ['matches' => $match])
         @endforeach
